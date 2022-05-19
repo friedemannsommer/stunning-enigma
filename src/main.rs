616acfd0172ad41b7dfd0808e34d1db08da46415
Friedemann Sommer <social@friedemannsommer.com>
2022-05-20 01:56:40 +0200

    initial setup

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
-use bevy::prelude::*;
+#![cfg_attr(all(not(debug_assertions), target_family = "windows"), windows_subsystem = "windows")]
+
+use bevy::app::App;
+use bevy::asset::AssetServer;
+use bevy::prelude::{Commands, OrthographicCameraBundle, Res, UiCameraBundle};
+use bevy::sprite::SpriteBundle;
+use bevy::DefaultPlugins;
+use mimalloc::MiMalloc;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
 
 fn main() {
     App::new()
@@ -9,6 +19,7 @@ fn main() {
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(UiCameraBundle::default());
     commands.spawn_bundle(SpriteBundle {
         texture: asset_server.load("icon.png"),
         ..Default::default()
