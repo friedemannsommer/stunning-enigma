846c49b9f8c6d28248afb6b6f740d0454a4942b5
Friedemann Sommer <social@friedemannsommer.com>
2022-05-26 18:20:46 +0200

    added exception(s) for "type_complexity" in "main_menu" systems

--- src/main_menu.rs
+++ src/main_menu.rs
@@ -77,6 +77,7 @@ pub fn setup_menu(mut commands: Commands, fonts: Res<FontAssets>) {
         });
 }
 
+#[allow(clippy::type_complexity)]
 pub fn button_interaction_visual(
     mut query: Query<(&Interaction, &mut UiColor), (Changed<Interaction>, With<Button>)>,
 ) {
@@ -95,6 +96,7 @@ pub fn button_interaction_visual(
     }
 }
 
+#[allow(clippy::type_complexity)]
 pub fn on_button_interaction<B: Component>(
     query: Query<&Interaction, (Changed<Interaction>, With<Button>, With<B>)>,
 ) -> bool {
